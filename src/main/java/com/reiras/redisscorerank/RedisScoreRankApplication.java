8c57ffabef7ca3205749a5a5445121b98df10afb
reirasdev <reiras.dev@gmail.com>
2020-06-06 18:37:01 -0300

    Initial project

---
+++ src/main/java/com/reiras/redisscorerank/RedisScoreRankApplication.java
@@ -0,0 +1,13 @@
+package com.reiras.redisscorerank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisScoreRankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedisScoreRankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/reiras/redisscorerank/RedisScoreRankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reiras.redisscorerank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisScoreRankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
